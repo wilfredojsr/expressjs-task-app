1a1f5848041cbae088000a77d1e8968f5738b18a
Wilfredo Suarez <wilfredojsr2@gmail.com>
2020-10-09 22:33:57 -0300

    Adding new connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 //const connectionURL = 'mongodb://<user>:encodeURIComponent('<pass>')@<host>:<port>/<database>?authSource=admin'
 const databaseName = 'task-manager'
-const connectionURL = 'mongodb://' + process.env.MONGO_DB_W + ':' + encodeURIComponent(process.env.MONGO_DB_X) + '@' + process.env.MONGO_DB_Y + ':' + process.env.MONGO_DB_Z + '/' + databaseName + '?authSource=admin'
+const connectionURL = process.env.MONGO_DB_URL
 
 mongoose.connect(connectionURL, {
   useUnifiedTopology: true,
